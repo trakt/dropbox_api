64f6c1ecd478e8d9edc2e6d54d651481dc3b25ce
Josef Šimánek <josef.simanek@gmail.com>
2021-02-06 20:31:36 +0100

    Require tempfile in specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'dropbox_api'
 require 'support/vcr'
+require 'tempfile'
 require File.expand_path('../support/dropbox_scaffold_builder', __FILE__)
 
 ENV["DROPBOX_OAUTH_BEARER"] ||= "MOCK_AUTHORIZATION_BEARER"
