472bb326128210d2939d32acf19af5f7abd4dd46
Jesus Burgos Macia <jesus@wave.com>
2021-02-07 11:15:30 +0100

    Re-ran rubocop after rebasing

--- spec/support/rake_tasks.rb
+++ spec/support/rake_tasks.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 require 'dropbox_api'
 require File.expand_path('../dropbox_scaffold_builder', __FILE__)
 
 namespace :test do
   # Example: `rake test:build_scaffold[get_metadata]`
-  desc "Regenerates the scaffolding required to test the given endpoint"
+  desc 'Regenerates the scaffolding required to test the given endpoint'
   task :build_scaffold, [:endpoint] => [:show_account_warning] do |t, args|
     if args[:endpoint].nil?
       puts 'Regenerating all'
@@ -15,8 +16,8 @@ namespace :test do
   end
 
   task :show_account_warning do
-    print "NOTE: This task will make changes on a real Dropbox account..."
+    print 'NOTE: This task will make changes on a real Dropbox account...'
     sleep 5
-    puts " ok, going ahead!"
+    puts ' ok, going ahead!'
   end
 end
