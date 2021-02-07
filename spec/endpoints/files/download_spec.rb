c5063def0b012c73f449192cb5178d0097ae186c
Jesus Burgos Macia <jesus@wave.com>
2021-02-07 11:09:50 +0100

    Fix tests

--- spec/endpoints/files/download_spec.rb
+++ spec/endpoints/files/download_spec.rb
@@ -14,10 +14,10 @@ describe DropboxApi::Client, '#download' do
   it 'yields the file contents', cassette: 'download/success' do
     file_contents = ''
     file = @client.download '/file.txt' do |chunk|
-      file_contents << chunk
+      file_contents = "#{file_contents}#{chunk}"
     end
 
-    expect(file_contents).to eq("Viva Rusia!\n")
+    expect(file_contents).to eq("Hola Leo!\n")
   end
 
   it 'raises an error if the name is invalid', cassette: 'download/not_found' do
